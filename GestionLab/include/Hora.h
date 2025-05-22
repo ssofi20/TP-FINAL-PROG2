f01bed76d183ccf3a29ea8605c631323e13ad004
BelenIr19 <bi40725@gmail.com>
2025-05-22 09:18:19 -0300

    Desarrollo clase hora

---
+++ GestionLab/include/Hora.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "cstring"
+
+
+class Hora
+{
+    private:
+
+        int _hora;
+        int _minuto;
+
+    public:
+        /// CONSTRUCTORES
+        Hora();
+        Hora(int hora, int minuto);
+
+        ///SETTERS
+
+        void setHora(int hora);
+        void setMinuto(int minuto);
+
+        /// GETTERS
+
+        int getHora();
+        int getMinuto();
+
+     /// Metodo de clase
+        std::string toString();
+
+};
+
