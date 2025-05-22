b63164aa37eb05bb179417f05965023470a7e63e
Sofia Bernigola <smbernigola@gmail.com>
2025-05-22 15:17:58 -0300

    constructores

--- GestionLab/include/Paciente.h
+++ GestionLab/include/Paciente.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "Fecha.h"
+#include <cstring>
             
 class Paciente
 {
     public:
         
         Paciente();
+        Paciente(int DNI, const char *nombre, const char *apellido, const char *telefono, const char *email, const char *obraSocial, const char *numeroAfiliado, Fecha dateB);
          
     private:
         int _DNI;
@@ -15,7 +17,7 @@ class Paciente
         char _email[60];
         char _obraSocial[50];
         char _numeroAfiliado[11];
-        Fecha _dateb;
+        Fecha _dateB;
         bool _estado;
 };
 
--- GestionLab/src/Paciente.cpp
+++ GestionLab/src/Paciente.cpp
@@ -1,5 +1,21 @@
 #include "Paciente.h"
 
 Paciente::Paciente(){
-   
+    
+    _DNI = 0;
+    strcpy(_nombre, "SIN NOMBRE");
+    strcpy(_apellido, "SIN APELLIDO");
+    strcpy(_telefono, "0");
+    strcpy(_email, "SIN EMAIL");
+    strcpy(_obraSocial, "SIN OS");
+    strcpy(_numeroAfiliado, "0");
+    _dateB.setDia(0);
+    _dateB.setMes(0);
+    _dateB.setAnio(0);
+    _estado = true;
+
+}
+Paciente::Paciente(int DNI, const char *nombre, const char *apellido, const char *telefono, const char *email, const char *obraSocial, const char *numeroAfiliado, Fecha dateB){
+            
+
 }
