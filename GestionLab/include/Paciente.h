3a6b4225305d6064be9cf9cd75706267a9b859fc
Sofia Bernigola <smbernigola@gmail.com>
2025-05-22 15:08:27 -0300

    Atributos e inclusión de Fecha

--- GestionLab/include/Paciente.h
+++ GestionLab/include/Paciente.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Fecha.h"
             
 class Paciente
 {
@@ -7,5 +8,14 @@ class Paciente
         Paciente();
          
     private:
+        int _DNI;
+        char _nombre[30];
+        char _apellido[40];
+        char _telefono[10];
+        char _email[60];
+        char _obraSocial[50];
+        char _numeroAfiliado[11];
+        Fecha _dateb;
+        bool _estado;
 };
 
