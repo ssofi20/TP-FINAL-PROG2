6a646b0073692661a0f04a60414b4638036b1f11
Sofia Bernigola <smbernigola@gmail.com>
2025-05-22 15:55:13 -0300

    getters y setters

--- GestionLab/include/TipoMuestra.h
+++ GestionLab/include/TipoMuestra.h
@@ -1,15 +1,26 @@
 #pragma once
-
+#include <cstring>
 
 class TipoMuestra
 {
     public:
         
         TipoMuestra();
+        
+        //SETTERS
+        void setIDMuestra(int IDMuestra);
+        void setNombre(const char *nombre);
+        void setEstado(bool estado);
+        
+        //GETTERS
+        int getIDMuestra() const;
+        const char * getNombre() const;
+        bool getEstado() const;
+        
 
     private:
         
-        int IDMuestra;
+        int _IDMuestra;
         char _nombre[35];
         bool _estado;
         
--- GestionLab/src/TipoMuestra.cpp
+++ GestionLab/src/TipoMuestra.cpp
@@ -3,3 +3,25 @@
 TipoMuestra::TipoMuestra(){
     
 }
+
+//SETTERS
+void TipoMuestra::setIDMuestra(int IDMuestra){
+    _IDMuestra = IDMuestra;
+}
+void TipoMuestra::setNombre(const char *nombre){
+    strcpy(_nombre, nombre);
+}
+void TipoMuestra::setEstado(bool estado){
+    _estado = estado;
+}
+
+//GETTERS
+int TipoMuestra::getIDMuestra() const{
+    return _IDMuestra;
+}
+const char * TipoMuestra::getNombre() const{
+    return _nombre;
+}
+bool TipoMuestra::getEstado() const{
+    return _estado;
+}
