4c4a10dfb1b25b153e5128fa5fbc9aa452a5801b
Sofia Bernigola <smbernigola@gmail.com>
2025-05-25 18:39:39 -0300

    Squashed commit of the following:
    commit ba481a5bbaccb3e1b460eb91525d7c9da7a53ae5
    Author: Sofia Bernigola <smbernigola@gmail.com>
    Date:   Sun May 25 18:24:48 2025 -0300
    
        Delete archivo temporal
    
    commit 89bb2d02d9b7c6a3acb7bdf8249373f9b40aa03f
    Author: Sofia Bernigola <smbernigola@gmail.com>
    Date:   Sun May 25 18:24:22 2025 -0300
    
        Update gitignore
    
    commit 9be9eb416746cc8e3fafdc483065d7e52e2747c5
    Merge: f810fe0 10b9b68
    Author: ssofigb <109748494+ssofi20@users.noreply.github.com>
    Date:   Sun May 25 17:22:39 2025 -0300
    
        Merge pull request #18 from ssofi20/features/clase-Turnos
    
        Features/clase turnos
    
    commit 10b9b68eafecaaf5c86b2dbfa3bd089a69e5a35c
    Author: BelenIr19 <bi40725@gmail.com>
    Date:   Sun May 25 17:20:41 2025 -0300
    
        Cambio set constructor sin parametros
    
    commit cf93d88ae1449d44fd53b4a658eab874771c4435
    Author: BelenIr19 <bi40725@gmail.com>
    Date:   Sun May 25 16:53:40 2025 -0300
    
        Agrego Fecha.h y Hora.h
    
    commit 32832cbd693de96dfad38debe638c6848ef6aeb1
    Merge: 33d67e1 10bb80b
    Author: BelenIr19 <118790170+BelenIr19@users.noreply.github.com>
    Date:   Sun May 25 16:05:30 2025 -0300
    
        Merge branch 'main' into features/clase-Turnos
    
    commit 33d67e105a112b8502c5dde66db67c26f6a6047d
    Author: BelenIr19 <bi40725@gmail.com>
    Date:   Sun May 25 16:03:19 2025 -0300
    
        Clase Turnos
    
    commit f810fe09cec8bc1c6d23623218ceff3a138de728
    Author: Sofia Bernigola <smbernigola@gmail.com>
    Date:   Fri May 23 19:00:14 2025 -0300
    
        update documentacion
    
    commit bef4b2f1e3d4ec5cb1280c1ce22ccf32fcb3e829
    Author: Sofia Bernigola <smbernigola@gmail.com>
    Date:   Fri May 23 13:21:57 2025 -0300
    
        Cracion de archivo de documentacion
    
    commit 852b477fa1d42bae5313573c2002759bf8d387fc
    Author: Sofia Bernigola <smbernigola@gmail.com>
    Date:   Thu May 22 19:34:51 2025 -0300
    
        Eliminar .depend y .layout
    
        Eliminar archivos generados y ahora ignorados por .gitignore
    
    commit 10bb80bb3102ee314852f94a647b09cd7eb31e42
    Author: Sofia Bernigola <smbernigola@gmail.com>
    Date:   Thu May 22 19:33:49 2025 -0300
    
        Deleted GestioLab.layout
    
        Eliminar archivos generados y ahora ignorados por .gitignore
    
    commit c224c05f4b92665ef8dfb840ab06c28db9558978
    Author: Sofia Bernigola <smbernigola@gmail.com>
    Date:   Thu May 22 19:27:18 2025 -0300
    
        Update gitignore
    
    commit 5732130f67763f88d15aa9498674180b6ee7875b
    Author: Sofia Bernigola <smbernigola@gmail.com>
    Date:   Thu May 22 19:25:52 2025 -0300
    
        Agregar .gitignore para ignorar archivos temporales y del IDE
    
    commit cdf105afa50acf87e9f520f3335ed768f213c12a
    Author: ssofigb <109748494+ssofi20@users.noreply.github.com>
    Date:   Thu May 22 14:55:56 2025 -0300
    
        Update README.md
    
    commit 41ec3cb132175fbddf46f9cf7a7b5531dd56b9dc
    Author: Sofia Bernigola <smbernigola@gmail.com>
    Date:   Wed May 21 16:57:56 2025 -0300
    
        Update GestionLab.layout

---
+++ GestionLab/include/Turnos.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Hora.h"
+#include "Fecha.h"
+
+class Turnos
+{
+    private:
+    int _IDTurno;
+    Fecha _fechaProgramada;
+    Hora _horaProgramada;
+    int _estadoTurno;
+
+
+    public:
+        /// Constructores
+
+        Turnos();
+        Turnos (int IDTurno, Fecha fechaProgramada, Hora horaProgramada, int estadoTurno);
+
+        /// Setters
+
+        void setIDTurno (int idTurno);
+        void setFechaProgramada(Fecha fechaProgramada);
+        void setHoraProgramada (Hora horaProgramada);
+        void setEstadoTurno (int estadoTurno);
+
+        /// Getters
+
+        int getIDTurno ();
+        Fecha getFechaProgramada();
+        Hora getHoraProgramada();
+        int getEstadoTurno();
+
+
+
+};
+
