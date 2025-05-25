933f4d24d8c93f97a8086fd97faaaf7dad8a521f
BelenIr19 <bi40725@gmail.com>
2025-05-25 20:25:08 -0300

    Clase TipoAnalisis

---
+++ GestionLab/include/TipoAnalisis.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <cstring>
+
+class TipoAnalisis
+{
+    private:
+    int _IDAnalisis;
+    int _IDMuestra;
+    char _nombre[60];
+    bool _estado;
+
+    public:
+        /// Constructores
+
+        TipoAnalisis();
+        TipoAnalisis(int idAnalisis, int idMuestra, const char *name, bool estado);
+
+        /// Setters
+
+        void setIDAnalisis(int idAnalisis);
+        void setIDMuestra(int idMuestra);
+        void setNombre(const char *name);
+        void setEstado(bool estado);
+
+        /// Getters
+
+        int getIDAnalisis();
+        int getIDMuestra();
+        const char *getNombre();
+        bool getEstado();
+};
+
