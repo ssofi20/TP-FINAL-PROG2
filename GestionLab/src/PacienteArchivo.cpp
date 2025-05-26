de156fcd6587639da7c62f36ce2717bc20d5328d
Sofia Bernigola <smbernigola@gmail.com>
2025-05-26 16:24:28 -0300

    metodo leer(int posicion)

--- GestionLab/src/PacienteArchivo.cpp
+++ GestionLab/src/PacienteArchivo.cpp
@@ -15,15 +15,27 @@ int PacienteArchivo::cantidadRegistros(){
     int cantidad = ftell(pFile) / sizeof(Paciente);
     fclose(pFile);
     return cantidad;
-
 }
+
+Paciente PacienteArchivo::leer(int posicion){
+    
+    FILE *pFile = fopen(_fileName.c_str(), "rb");
+    if(pFile == nullptr){
+        return Paciente();
+    }
+    Paciente paciente;
+    fseek(pFile, sizeof(Paciente) * posicion, SEEK_SET);
+    fread(&paciente, sizeof(Paciente), 1, pFile);
+    fclose(pFile);
+    return paciente;
+}
+
+void PacienteArchivo::leer(int cantidadRegistros, Paciente *vecPaciente){
+}
+
 int PacienteArchivo::buscar(int DNI){
 }
 bool PacienteArchivo::guardar(Paciente paciente){
 }
 bool PacienteArchivo::guardar(Paciente paciente, int posicion){
-}
-Paciente PacienteArchivo::leer(int posicion){
-}
-void PacienteArchivo::leer(int cantidadRegistros, Paciente *vecPaciente){
 }
