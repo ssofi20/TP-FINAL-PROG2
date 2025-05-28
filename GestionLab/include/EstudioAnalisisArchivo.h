4b02246b7dea3ccf26ea4647d6d18b8fdeee00a4
Sofia Bernigola <smbernigola@gmail.com>
2025-05-28 08:50:40 -0300

    Creacion de archivos

---
+++ GestionLab/include/EstudioAnalisisArchivo.h
@@ -0,0 +1,13 @@
+#ifndef ESTUDIOANALISISARCHIVO_H
+#define ESTUDIOANALISISARCHIVO_H
+
+
+class EstudioAnalisisArchivo
+{
+    public:
+        EstudioAnalisisArchivo();
+
+    private:
+};
+
+#endif // ESTUDIOANALISISARCHIVO_H
---
+++ GestionLab/src/EstudioAnalisisArchivo.cpp
@@ -0,0 +1,5 @@
+#include "EstudioAnalisisArchivo.h"
+
+EstudioAnalisisArchivo::EstudioAnalisisArchivo(){
+
+}
