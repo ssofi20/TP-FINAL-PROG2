1c7d2eb28950009e8fdc4de06fe5c93c8c931712
BelenIr19 <bi40725@gmail.com>
2025-05-31 23:39:43 -0300

    Metodos cargar y mostrar de la clase Estudio

---
+++ GestionLab/include/EstudioManager.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "EstudioArchivo.h"
+#include "Estudio.h"
+
+class EstudioManager
+{
+private:
+    EstudioArchivo _archivo;
+
+public:
+
+    ///Registrar nuevo estudio.
+    void opcion1();
+    /// Eliminar estudios
+    void opcion2();
+
+    /// Sofi
+    void opcion3();
+    void opcion4();
+
+};
+
---
+++ GestionLab/src/EstudioManager.cpp
@@ -0,0 +1,16 @@
+#include "EstudioManager.h"
+
+    ///Registrar nuevo estudio.
+    void EstudioManager::opcion1()
+    {
+        Estudio obj;
+
+
+
+    }
+
+    /// Eliminar estudios
+    void EstudioManager::opcion2()
+    {
+
+    }
