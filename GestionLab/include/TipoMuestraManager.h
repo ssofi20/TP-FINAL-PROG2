c4e6ba3d6677a19f3e4fe9d6f8881624fb30f11a
Sofia Bernigola <smbernigola@gmail.com>
2025-06-05 09:18:40 -0300

    creacion de los archivos

---
+++ GestionLab/include/TipoMuestraManager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class TipoMuestraManager
+{
+    public:
+        void opcion1();
+        void opcion2();
+        void opcion3();
+        void opcion4();
+    private:
+};
+
---
+++ GestionLab/src/TipoMuestraManager.cpp
@@ -0,0 +1,17 @@
+#include "TipoMuestraManager.h"
+
+void TipoMuestraManager::opcion1(){
+
+}
+
+void TipoMuestraManager::opcion2(){
+
+}
+
+void TipoMuestraManager::opcion3(){
+
+}
+
+void TipoMuestraManager::opcion4(){
+
+}
